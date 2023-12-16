3bfefa5614a06f5a792b0f79952ef28356dc5f82
So Youn Choi <manghatuhyo@gmail.com>
2023-12-16 10:59:58 +0100

    testing not working so rollback

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,9 +4,7 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI)
 
-        // console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
-        // test without color
-        console.log(`MongoDB Connected: ${conn.connection.host}`)
+        console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
     } catch (error) {
         console.log(error)
         process.exit(1)
